940a22d311d3920d7fb6035d35bf0acfd4ea29ff
Loïc GIRON DIT METAZ <loic.gironditmetaz@gmail.com>
2024-10-02 15:36:09 +0200

    Display SDK updated to version 7.24.2

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SASDisplayKit",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v12)
     ],
     products: [
         .library(
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SASDisplayKit",
-            url: "https://sdk.sascdn.com/mobile/displaysdk/ios/SASDisplayKit-8.2.1.zip",
-            checksum: "98a41d7b28f47ce7a0d671c57c3cb7b0da59bd90e309f50acf9d9b49532a10ec"),
+            url: "https://sdk.sascdn.com/mobile/displaysdk/ios/SASDisplayKit-7.24.2.zip",
+            checksum: "642fb54fd9babfc41b7f157151e02904981c4e72b388b05a2da8d9e882575783"),
     ]
 )
