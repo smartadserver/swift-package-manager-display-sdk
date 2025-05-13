47aa4b68782f42325a0e036422cf6e54e7fd8496
Loïc GIRON DIT METAZ <lgironditmetaz@smartadserver.com>
2025-05-13 10:52:19 +0200

    Display SDK updated to version 8.5.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SASDisplayKit",
-            url: "https://sdk.sascdn.com/mobile/displaysdk/ios/SASDisplayKit-8.4.0.zip",
-            checksum: "e642df8cf319519d584b74cd1083c65de017e1981a55ab1a5ac4f527deb303e7"),
+            url: "https://sdk.sascdn.com/mobile/displaysdk/ios/SASDisplayKit-8.5.0.zip",
+            checksum: "5cd8c226d4d967626f38ed19f296c42f7568cbd0b5387ba25432e6ca53df78a7"),
     ]
 )
